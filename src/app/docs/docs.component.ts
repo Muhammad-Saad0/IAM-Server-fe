d35a9a468eb29bb97e664ad097bc80efc9fdbc09
Muhammad Saad <muhammadsaad3498@gmail.com>
2026-06-18 21:59:35 +0500

    ui: added taiga ui, docs page rework

--- src/app/docs/docs.component.ts
+++ src/app/docs/docs.component.ts
@@ -1,7 +1,20 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TuiButton, TuiIcon, TuiNotification, TuiTitle } from '@taiga-ui/core';
+import { TuiBadge } from '@taiga-ui/kit';
+import { TuiCardLarge, TuiHeader, TuiSurface } from '@taiga-ui/layout';
 
 @Component({
   selector: 'app-docs',
+  imports: [
+    TuiBadge,
+    TuiButton,
+    TuiCardLarge,
+    TuiHeader,
+    TuiIcon,
+    TuiNotification,
+    TuiSurface,
+    TuiTitle,
+  ],
   templateUrl: './docs.component.html',
   styleUrl: './docs.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
