c58697c5caf80966a0d50a42b23966c6554655bc
Muhammad Saad <muhammadsaad3498@gmail.com>
2026-06-20 01:53:29 +0500

    fix: correct dashboard text rendering

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { TuiButton, TuiIcon, TuiTitle } from '@taiga-ui/core';
+import { TuiButton, TuiIcon } from '@taiga-ui/core';
 import { TuiBadge } from '@taiga-ui/kit';
-import { TuiCardLarge, TuiHeader, TuiSurface } from '@taiga-ui/layout';
+import { TuiCardLarge, TuiSurface } from '@taiga-ui/layout';
 
 import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-home',
-  imports: [
-    RouterLink,
-    TuiBadge,
-    TuiButton,
-    TuiCardLarge,
-    TuiHeader,
-    TuiIcon,
-    TuiSurface,
-    TuiTitle,
-  ],
+  imports: [RouterLink, TuiBadge, TuiButton, TuiCardLarge, TuiIcon, TuiSurface],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
